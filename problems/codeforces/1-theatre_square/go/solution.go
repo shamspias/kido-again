4879f020c5762c9b96e657f5899a6c9eb757b7e0
Shamsuddin Ahmed <shamsuddinpias0@gmail.com>
2026-02-07 16:07:45 +0600

    solve: 1 with go

--- problems/codeforces/1-theatre_square/go/solution.go
+++ problems/codeforces/1-theatre_square/go/solution.go
@@ -2,10 +2,15 @@ package main
 
 import "fmt"
 
+func calculatePlace(m, n, a int64) int64 {
+	mPart := (m + a - 1) / a
+	nPart := (n + a - 1) / a
+	return mPart * nPart
+}
 func main() {
-	var a, b, c int
-	if _, err := fmt.Scan(&a, &b, &c); err != nil {
+	var m, n, a int64
+	if _, err := fmt.Scan(&m, &n, &a); err != nil {
 		return
 	}
-
+	fmt.Println(calculatePlace(m, n, a))
 }
