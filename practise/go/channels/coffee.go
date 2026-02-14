70fafa06db5825d5bf9e489d5eb5faca71255cb8
Shamsuddin Ahmed <shamsuddinpias0@gmail.com>
2026-02-14 20:54:46 +0600

    learn: channel

--- practise/go/channels/coffee.go
+++ practise/go/channels/coffee.go
@@ -1,28 +1,15 @@
 package main
 
-import (
-	"fmt"
-)
+import "fmt"
 
-func makeCoffee(order string, counter chan string) {
-	// Do the work
+func makeCoffee2(order string, counter chan string) {
 	drink := "Hot " + order
-
-	// Send the finished drink to the counter (Channel)
-	// The arrow points INTO the channel
 	counter <- drink
 }
 
 func main() {
-	// 1. Build the Counter (Channel) that holds strings
 	pickupCounter := make(chan string)
-
-	// 2. Tell the barista to make coffee and put it on the 'pickupCounter'
-	go makeCoffee("Latte", pickupCounter)
-
-	// 3. You wait at the counter to receive it
-	// The arrow points OUT of the channel
+	go makeCoffee2("Latte", pickupCounter)
 	servedDrink := <-pickupCounter
-
 	fmt.Println("Manager received:", servedDrink)
 }
